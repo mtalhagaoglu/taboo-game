da3ff4b0317cb4a1dbd4cb0753a332b06b365593
mtalhagaoglu <47723198+mtalhagaoglu@users.noreply.github.com>
2023-01-28 22:16:09 -0800

    Update fbConfig.js

--- src/utils/fbConfig.js
+++ src/utils/fbConfig.js
@@ -4,15 +4,15 @@ import "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DB_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-}
+  apiKey: "AIzaSyB_Cd2Uw8cJy7luKpVXZbmP2Oh2NcM6FbQ",
+  authDomain: "taboo-game-8e775.firebaseapp.com",
+  databaseURL: "https://taboo-game-8e775-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "taboo-game-8e775",
+  storageBucket: "taboo-game-8e775.appspot.com",
+  messagingSenderId: "211659342195",
+  appId: "1:211659342195:web:1d7bf04cd848283ceb4506",
+  measurementId: "G-FZ399CPWCN"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
